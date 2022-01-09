6b17c79e3ddebf5f1d323bc5ac776e2a951648bf
Jonathan Long <jonathanelong@icloud.com>
2022-01-09 13:50:25 -0600

    Update App Icon to work better with smaller sizes.

---
+++ LonesomeDove/Views/LoadingView.swift
@@ -0,0 +1,20 @@
+//
+//  LoadingView.swift
+//  LonesomeDove
+//
+//  Created by Jonathan Long on 1/9/22.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoadingView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoadingView()
+    }
+}
